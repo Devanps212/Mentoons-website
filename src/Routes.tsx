4f4ed44ea79df4b9e859f8f5227c7487e614ea6f
Devan P S <devanps212@gmail.com>
2026-05-13 16:14:01 +0530

    try 2

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -59,10 +59,10 @@ import Feedback from "./pages/v2/feedback/feedback.tsx";
 import Collaborate from "./pages/v2/joinus/collaborate/collaborate.tsx";
 import BecomeMentor from "./pages/v2/joinus/becomeMentor.tsx";
 import MessageFromFounder from "./pages/v2/MessageFromFounder.tsx";
+import NewLandingPage from "./pages/v2/newLandingPage.tsx";
 // import NewHome from "./pages/v2/home/home.tsx";
 // import WorkshopV2 from "./pages/v2/workshop/workshopV2.tsx";
 
-const NewLandingPage = lazy(() => import("./pages/v2/newLandingPage"));
 const Cart = lazy(() => import("./pages/Cart"));
 const ComicsPageV2 = lazy(() => import("./pages/ComicsPageV2"));
 const Podcastv2 = lazy(() => import("./pages/Podcastv2"));
