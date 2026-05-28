04ef8ad83f855836ae8193e8abaff3faa4848559
Devan P S <devanps212@gmail.com>
2026-05-28 15:15:32 +0530

    friend request error fixed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -59,7 +59,7 @@ import Feedback from "./pages/v2/feedback/feedback.tsx";
 import Collaborate from "./pages/v2/joinus/collaborate/collaborate.tsx";
 import BecomeMentor from "./pages/v2/joinus/becomeMentor.tsx";
 import MessageFromFounder from "./pages/v2/MessageFromFounder.tsx";
-import NewLandingPage from "./pages/v2/newLandingPage";
+import NewLandingPage from "./pages/v2/newLandingPage.tsx";
 // import NewHome from "./pages/v2/home/home.tsx";
 // import WorkshopV2 from "./pages/v2/workshop/workshopV2.tsx";
 
